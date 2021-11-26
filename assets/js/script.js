41b13d6371d85c1d655912f0f62f3c296ffce7fc
_watercolor <kimura@lopan.jp>
2021-11-26 19:16:02 +0900

    refine211126

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,18 @@
 	function onLoaded() {
 		addFnc([
 			webFontLoader,
+			windowManagement,
 			transformScroll,
+			magneticButton,
 			setInview,
+			setLazy,
 			drawerMenu,
 			anchorScroll,
 			setupYouTube
 		]);
+		if (location.search) {
+			console.log( returnObject(location.search.substring(1)) );
+		}
 		if (document.querySelector('path') !== null) setPathLen();
 		loop();
 	}
@@ -19,18 +25,32 @@
 	function onResize() {
 		resized(function() {
 			if (as.duration !== undefined) as.onResize();
+			if (wm.yOffset !== undefined) wm.refresh();
 		});
 	}
 	function onScroll() {
 		throttle(function() {
-			console.log(window.pageYOffset);
+// 			console.log(window.pageYOffset);
 		});
 	}
+	/* マッピング (パララックス)
+	 * value が fromMin から toMin へ変化する間 fromMax から toMax を返す
+	 */
+	function map (value, fromMin, fromMax, toMin, toMax, trimming) {
+		var val = (value - fromMin) / (fromMax - fromMin) * (toMax - toMin) + toMin;
+		return trimming ? Math.max(Math.min(val, toMax), toMin) : val;
+	}
+	const movie = document.getElementsByClassName('c-video')[0];
 	function loop() {
+		const a = map(window.pageYOffset, 400, 0, 0, 1000, true);
+		movie.style.opacity = a / 1000;
 		requestAnimationFrame(loop);
 		if (ts.wrapper !== undefined) {
 			ts.onScroll();
 			ts.insistentRefresh();
+		}
+		if (wm.yOffset !== undefined) {
+			wm.yOffset = ts.wrapper !== undefined ? ts.posY : window.pageYOffset;
 		}
 	}
 	
@@ -49,8 +69,9 @@
 		constructor: function() {
 			//console.log('-----> webFontLoader');
 			window.WebFontConfig = {
-				google: {
-					families: ['Lato:400,700', 'Noto+Sans+JP:400,700', 'Noto+Serif+JP']
+				custom: {
+					families: [ 'Lato', 'Noto+Sans+JP' ],
+					urls: [ 'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap' ]
 				},
 				loading: function() {
 					console.log('loading');
@@ -71,6 +92,128 @@
 	};
 	
 	/**
+	 * MagneticButton
+	 -------------------------------------------------- */
+	function magneticButton() {
+		mgbtn.constructor();
+	}
+	const mgbtn = {
+		constructor: function() {
+			this.winsize = this.calcWinsize();
+			this.cursorpos = { x: 0, y: 0 };
+			window.addEventListener('mousemove', function(e) {
+				mgbtn.cursorpos = { x: e.clientX, y: e.clientY };
+			});
+			this.btnsData = {};
+			
+			const btns = document.getElementsByClassName('js-mgBtn');
+			for (let i = 0, len = btns.length; i < len; i++) {
+				btns[i].dataset['mg'] = 'btn-' + (i + 1);
+				btns[i].innerHTML = '<span class="text">' + btns[i].innerHTML + '</span>';
+				this.setData(btns[i], 'btn-' + (i + 1));
+				btns[i].addEventListener('mouseenter', this.enter);
+				btns[i].addEventListener('mouseleave', this.leave);
+			}
+			//console.log(this.btnsData);
+			this.loop();
+		},
+		setData: function(btn, id) {
+			const rect = btn.getBoundingClientRect();
+			const top = rect.top + window.pageYOffset;
+			this.btnsData[id] = {
+				isHov: false,
+				el: btn,
+				txt: btn.firstChild,
+				rect: {
+					top: rect.top + window.pageYOffset,
+					left: rect.left,
+					width: rect.width,
+					height: rect.height
+				},
+				renderedStyles: {
+					tx: { pre: 0, cur: 0, amt: .1 },
+					ty: { pre: 0, cur: 0, amt: .1 }
+				},
+				distanceToTrigger: rect.width / 1.5
+			};
+			console.log(this.btnsData[id].rect.top);
+		},
+		resetData: function() {
+			let rect;
+			for (let i in this.btnsData) {
+				rect = this.btnsData[i].el.getBoundingClientRect();
+				this.btnsData[i].rect = {
+					top: rect.top + window.pageYOffset,
+					left: rect.left,
+					width: rect.width,
+					height: rect.height
+				};
+				this.btnsData[i].distanceToTrigger = rect.width / 1.5;
+				console.log(i, this.btnsData[i].rect.top);
+			}
+		},
+		lerp: function(a, b, n) {
+			return (1 - n) * a + n * b;
+		},
+		calcWinsize: function() {
+			return { width: window.innerWidth, height: window.innerHeight };
+		},
+		distance: function(x1, y1, x2, y2) {
+			const a = x1 - x2;
+			const b = y1 - y2;
+			return Math.hypot(a, b);
+		},
+		loop: function() {
+			for (let i in mgbtn.btnsData) {
+				mgbtn.render(mgbtn.btnsData[i]);
+			}
+			requestAnimationFrame(mgbtn.loop);
+		},
+		enter(e) {
+			mgbtn.btnsData[this.dataset['mg']].isHov = true;
+		},
+		leave(e) {
+			mgbtn.btnsData[this.dataset['mg']].isHov = false;
+		},
+		render(data) {
+			//if (data.isHov) console.log(data.el.dataset['mg']);
+			let x = 0, y = 0;
+			
+			// カーソルからボタンの中心までの距離
+			const distanceMouseButton = mgbtn.distance(
+				mgbtn.cursorpos.x + window.scrollX,
+				mgbtn.cursorpos.y + window.scrollY,
+				data.rect.left + data.rect.width / 2,
+				data.rect.top + data.rect.height / 2
+			);
+			if (distanceMouseButton < data.distanceToTrigger) {
+				x = (mgbtn.cursorpos.x + window.scrollX - (data.rect.left + data.rect.width / 2)) * .3;
+				y = (mgbtn.cursorpos.y + window.scrollY - (data.rect.top + data.rect.height / 2)) * .3;
+			}
+			data.renderedStyles['tx'].cur = x;
+			data.renderedStyles['ty'].cur = y;
+			
+			for (let key in data.renderedStyles) {
+				data.renderedStyles[key].pre = mgbtn.lerp(
+					data.renderedStyles[key].pre,
+					data.renderedStyles[key].cur,
+					data.renderedStyles[key].amt
+				);
+			}
+			const tx = data.renderedStyles['tx'].pre;
+			const ty = data.renderedStyles['ty'].pre;
+			
+			if (Math.abs(tx) >= 0.01 || Math.abs(ty) >= 0.01) {
+				data.el.style.transform = 'translate3d(' + tx + 'px,' + ty + 'px,0)';
+				data.txt.style.transform = 'translate3d(' + -tx / 2 + 'px,' + -ty / 2 + 'px,0)';
+			} else {
+				data.el.removeAttribute('style');
+				data.txt.removeAttribute('style');
+			}
+		}
+	};
+	
+	/**
 	 * .js-sc-wrap 内のスクロールを transform: translate に置き換える
 	 * transformScroll
 	 -------------------------------------------------- */
@@ -86,7 +229,6 @@
 			this.dummy = document.getElementsByClassName('js-sc-dummy')[0];
 			this.wrapper = document.getElementsByClassName('js-sc-wrap')[0];
 			this.wrapper.classList.add('js-fixed');
-			this.winH = window.innerHeight;
 			
 			this.frictionOrigin = 10 / 100;
 			this.ratioOrigin = 60 / 10;
@@ -155,7 +297,7 @@
 			// ずれる要素の位置
 			if (this.slipperData !== undefined) {
 				let data;
-				const diff = this.winH / 2;
+				const diff = wm.winH / 2;
 				for (var i = this.slipper.length; i--;) {
 					data = this.slipperData[i];
 					data.posY += (goalY + diff - data.posY) * (this.friction * 0.8);
@@ -202,52 +344,75 @@
 	const dm = {
 		constructor: function() {
 			//console.log('-----> drawerMenu');
-			this.yOffset = window.pageYOffset;
 			this.menu = document.getElementsByClassName('l-menu')[0];
-			this.btn = document.getElementsByClassName('js-menuToggle');
-			const btn = document.querySelectorAll('.js-menuToggle, .l-nav .overlay');
+			const btn = document.getElementsByClassName('js-menuToggle');
 			for (let i = 0, len = btn.length; i < len; i++) {
 				btn[i].addEventListener('click', this.toggleMenu);
 			}
-			for (let i = 0, len = this.btn.length; i < len; i++) {
-				this.btn[i].addEventListener('transitionend', this.btnEnd);
-			}
-			this.menu.addEventListener('transitionend', this.menuEnd);
 		},
 		toggleMenu: function() {
 			!dm.isOpened ? dm.openMenu() : dm.closeMenu();
+			dm.menu.addEventListener('transitionend', dm.menuEnd);
 		},
 		openMenu: function() {
+// 			console.log('openMenu', wm.yOffset);
 			dm.isOpened = true;
-			dm.yOffset = window.pageYOffset;
-			//console.log('openMenu', dm.yOffset);
-			document.body.classList.add('is-fixed');
-			document.body.style.marginTop = -dm.yOffset + 'px';
-			for (let i = 0, len = dm.btn.length; i < len; i++) {
-				dm.btn[i].classList.add('is-active', 'is-anim');
-			}
+			wm.posLock();
 			dm.menu.classList.add('is-active', 'is-anim');
 		},
 		closeMenu: function() {
-			//console.log('closeMenu', dm.yOffset);
-			document.body.classList.remove('is-fixed');
-			document.body.removeAttribute('style');
-			window.scrollTo(0, dm.yOffset);
+// 			console.log('closeMenu', wm.yOffset);
 			dm.isOpened = false;
-			for (let i = 0, len = dm.btn.length; i < len; i++) {
-				dm.btn[i].classList.replace('is-active', 'is-anim');
-			}
+			wm.posUnlock();
 			dm.menu.classList.replace('is-active', 'is-anim');
 		},
-		btnEnd: function(e) {
-			if (e.propertyName === 'transform') {
-				this.classList.remove('is-anim');
-			}
-		},
 		menuEnd: function(e) {
-			if (e.propertyName === 'opacity') {
-				this.classList.remove('is-anim');
+			if (dm.isOpened) {
+				if (e.target.className.indexOf('close') >= 0) {
+					this.classList.remove('is-anim');
+					dm.menu.removeEventListener('transitionend', dm.menuEnd);
+				}
+			} else {
+				if (e.target.className.indexOf('overlay') >= 0) {
+					this.classList.remove('is-anim');
+					dm.menu.removeEventListener('transitionend', dm.menuEnd);
+				}
 			}
+		}
+	};
+	
+	/**
+	 * window の情報を管理する
+	 * モーダルが開いた時とかはここで画面をロックする
+	 * windowManagement
+	 -------------------------------------------------- */
+	function windowManagement() {
+		wm.constructor();
+	}
+	const wm = {
+		constructor: function() {
+			this.body = document.body;
+			this.refresh();
+		},
+		refresh: function() {
+			this.winH = window.innerHeight;
+			this.yOffset = ts.wrapper !== undefined ? ts.posY : window.pageYOffset;
+		},
+		posLock: function() {
+			wm.isLock = 1;
+			if (ts.wrapper !== undefined) ts.isLock = wm.isLock;
+			wm.lockOffset = wm.yOffset;
+			wm.body.classList.add('is-fixed');
+			wm.body.style.marginTop = -wm.lockOffset + 'px';
+		},
+		posUnlock: function() {
+			wm.body.classList.remove('is-fixed');
+			wm.body.removeAttribute('style');
+			window.scrollTo(0, wm.lockOffset);
+			setTimeout(function() {
+				wm.isLock = 0;
+				if (ts.wrapper !== undefined) ts.isLock = wm.isLock;
+			}, 60);
 		}
 	};
 	
@@ -266,8 +431,7 @@
 				setTimeout(function() {
 					as.toScroll(location.hash.slice(1));
 					history.replaceState('', document.title, window.location.pathname);
-					//history.replaceState(null, null, './');
-				}, 60);
+				}, 80);
 			}
 			this.SPF = 1000 / 60;
 			this.duration = 800;
@@ -280,12 +444,12 @@
 		},
 		onClick: function(e) {
 			e.preventDefault();
-			const targetId = this.closest('[href]').getAttribute('href');
+			const targetId = this.getAttribute('href');
 			as.toScroll(targetId.slice(1));
 		},
 		onResize: function() {
 			as.scBtm = document.body.offsetHeight - window.innerHeight;
-			as.scPad = document.getElementsByClassName('l-header')[0].clientHeight - 1;
+			as.scPad = document.getElementsByClassName('l-header')[0].clientHeight;
 			as.scMax = document.body.clientHeight - window.innerHeight;
 			
 			// body の高さが取得できなかった場合 0.06 秒後に改めて実行
@@ -293,20 +457,21 @@
 		},
 		toScroll: function(targetId) {
 			if (targetId !== 'top' && document.getElementById(targetId) === null) return;
+			//console.log(targetId);
 			
 			// ドロワーメニューが開いていたら、開いた時の位置からスタート (メニューは閉じる)
-			this.startPos = (dm.isOpened) ? dm.yOffset : window.pageYOffset;
+			this.startPos = (dm.isOpened) ? wm.lockOffset : wm.yOffset;
 			if (dm.isOpened) dm.closeMenu();
 			
 			this.currentPos = this.startPos;
 			this.elapsedTime = 0;
-			this.goalPos = (targetId !== 'top') ? (document.getElementById(targetId).getBoundingClientRect().top + window.pageYOffset - this.scPad) : 0;
-			//console.log(this.startPos, window.pageYOffset);
+			this.goalPos = (targetId !== 'top') ? (document.getElementById(targetId).getBoundingClientRect().top + wm.yOffset - this.scPad) : 0;
+			//console.log(targetId, this.startPos, this.goalPos);
 			if (this.goalPos >= this.scMax) {
 				this.goalPos = this.scMax;
 			}
 			this.valueInChange = this.goalPos - this.startPos;
-			//this.duration = Math.abs(this.valueInChange) < 1200 ? 600 : Math.abs(this.valueInChange) / 2;
+			this.duration = Math.abs(this.valueInChange) < 1200 ? 600 : Math.abs(this.valueInChange) / 2;
 			//console.log(this.startPos, this.goalPos, this.valueInChange);
 			this.doScroll();
 		},
@@ -319,11 +484,11 @@
 			
 			if (as.elapsedTime >= as.duration) {
 				as.currentPos = as.goalPos;
-				clearTimeout(as.timer);
+				cancelAnimationFrame(as.timer);
 				return;
 			} else {
 				as.elapsedTime += as.SPF;
-				as.timer = setTimeout(as.doScroll, as.SPF);
+				as.timer = requestAnimationFrame(as.doScroll);
 			}
 			
 			window.scrollTo(0, as.currentPos);
@@ -388,7 +553,7 @@
 				//console.dir(ytElms[i]);
 				id = 'player-' + i;
 				ytid = ytElms[i].dataset['ytid'];
-				console.dir(ytElms[i]);
+// 				console.dir(ytElms[i]);
 				w = ytElms[i].getAttribute('width') ? ytElms[i].getAttribute('width') : 560;
 				h = ytElms[i].getAttribute('height') ? ytElms[i].getAttribute('height') : 315;
 				ytElms[i].setAttribute('id', id);
@@ -436,18 +601,8 @@
 	};
 	
 	/**
-	 * fetch が使えたら fetch、使えなかったら xhr で Ajax する
-	 * doAjax(request, function() {}, 'text');
 	 * fetchAjax(request, function() {}, 'json');
-	 * xhrAjax(request, function() {}, 'blob');
 	 * -------------------------------------------------- */
-	function doAjax(req, fn, format) {
-		if (self.fetch) {
-			fetchAjax(req, fn, format)
-		} else {
-			xhrAjax(req, fn, format)
-		}
-	}
 	function fetchAjax(req, fn, format) {
 		//console.log('-----> fetchAjax');
 		fetch(req)
@@ -468,46 +623,39 @@
 			console.log('error -----> '.concat(error));
 		});
 	};
-	function xhrAjax(req, fn, format) {
-		//console.log('-----> xhrAjax');
-		const xhr = new XMLHttpRequest();
-		xhr.responseType = format;
-		xhr.addEventListener('progress', function(e) {
-			const rate = e.loaded / (e.total == 0 ? e.loaded : e.total);
-			console.log(e, rate);
-		});
-		xhr.addEventListener('load', function(e) {
-			if (xhr.readyState === 4) {
-				if (xhr.status == 200 || xhr.status == 304) {
-					let data = e.target.response;
-					if (format === 'json') data = JSON.parse(data);
-					fn(data);
-				}
-			}
-		});
-		xhr.open('GET', req);
-		xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
-		xhr.send();
-	};
 	
 	/**
 	 * in-view
-	 * <ul class="js-inview" data-last="li:last-child"></ul>
-	 * <ul class="js-inview" data-last><li></li><li></li><li class="iv-last"></li></ul>
+	 * <ul class="js-inview" data-last=":last-child"></ul>
 	 * <ul class="js-inview" data-anim="animationName"></ul>
-	 * <img data-src="path.jpg" alt="" width="w" height="h">
 	 * -------------------------------------------------- */
 	function setInview() {
-		iv.constructor();
+		if (document.querySelector('.js-inview') !== null) iv.constructor();
 	}
 	const iv = {
 		constructor: function() {
-			if (document.querySelector('.js-inview') !== null) iv.basic();
-			if (document.querySelector('[data-src]') !== null) iv.lazySetup();
+			//inView('.js-inview').on('enter', this.doInview);
+			//inView.offset({ top: 80, right: 0, bottom: 80, left: 0 });
+			const elms = document.getElementsByClassName('js-inview');
+			if (self.IntersectionObserver !== undefined) {
+				console.log('IntersectionObserver');
+				this.inview(elms);
+			} else {
+				console.log('スクロールで実装');
+			}
 		},
-		basic: function() {
-			inView('.js-inview').on('enter', this.doInview);
-			inView.offset({ top: 80, right: 0, bottom: 80, left: 0 });
+		inview: function(elms) {
+			this.intersectionObserver = new IntersectionObserver(function(entries) {
+				for (let i = 0; i < entries.length; i++) {
+					if (entries[i].intersectionRatio <= 0) continue;
+					iv.doInview(entries[i].target);
+				}
+			}, {
+				rootMargin: '-80px 0px -80px 0px'
+			});
+			for (let i = 0; i < elms.length; i++) {
+				this.intersectionObserver.observe(elms[i]);
+			}
 		},
 		doInview: function(el) {
 			if (!el.isInview) {
@@ -531,19 +679,10 @@
 			}
 		},
 		lastElmEnd: function(e) {
-			if (this.dataset['last'] !== '') {
-				if (e.target === this.querySelector(this.dataset['last'])) {
-					this.classList.remove('is-anim', 'js-inview');
-					this.removeAttribute('data-last');
-					this.removeEventListener('transitionend', iv.lastElmEnd);
-				}
-			} else {
-				if (e.target.className.indexOf('iv-last') >= 0) {
-					e.target.classList.remove('iv-last');
-					this.classList.remove('is-anim', 'js-inview');
-					this.removeAttribute('data-last');
-					this.removeEventListener('transitionend', iv.lastElmEnd);
-				}
+			if (e.target === this.querySelector(this.dataset['last'])) {
+				this.classList.remove('is-anim', 'js-inview');
+				this.removeAttribute('data-last');
+				this.removeEventListener('transitionend', iv.lastElmEnd);
 			}
 		},
 		transitionendEnd: function(e) {
@@ -551,9 +690,18 @@
 				this.classList.remove('is-anim', 'js-inview');
 				this.removeEventListener('transitionend', iv.transitionendEnd);
 			}
-		},
-		// lazyLoader & inview
-		lazySetup: function() {
+		}
+	};
+	
+	/**
+	 * lazy image
+	 * <img data-src="path.jpg" alt="" width="w" height="h">
+	 * -------------------------------------------------- */
+	function setLazy() {
+		if (document.querySelector('[data-src]') !== null) lz.constructor();
+	}
+	const lz = {
+		constructor: function() {
 			let cls, w, h;
 			const imgs = document.querySelectorAll('img[data-src]');
 			for (let i = 0, len = imgs.length; i < len; i++) {
@@ -562,30 +710,53 @@
 				h = imgs[i].getAttribute('height');
 				imgs[i].src = 'data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 ' + w + ' ' + h + '%22%3E%3C/svg%3E';
 			}
-			this.lazy();
-		},
-		lazy: function() {
-			inView('img[data-src]').on('enter', this.doLazy);
-			inView.offset({ top: 80, right: 0, bottom: 80, left: 0 });
-		},
-		doLazy: function(el) {
-			if (!el.isInview) {
-				imagesLoaded(el, iv.imageLoaded);
-				el.src = el.dataset['src'];
-				el.isInview = 1;
+			
+			if (self.IntersectionObserver !== undefined) {
+				console.log('IntersectionObserver');
+				this.lazy(imgs);
+			} else {
+				console.log('スクロールで実装');
 			}
 		},
-		imageLoaded: function() {
+		lazy: function(imgs) {
+			this.intersectionObserver = new IntersectionObserver(function(entries) {
+				for (let i = 0; i < entries.length; i++) {
+					if (entries[i].intersectionRatio <= 0) continue;
+					lz.doLazy(entries[i].target);
+				}
+			}, {
+				rootMargin: '-80px 0px -80px 0px'
+			});
+			for (let i = 0; i < imgs.length; i++) {
+				this.intersectionObserver.observe(imgs[i]);
+			}
+		},
+		doLazy: function(el) {
+			imagesLoaded(el, lz.loaded);
+			el.src = el.dataset['src'];
+			lz.intersectionObserver.unobserve(el);
+		},
+		loaded: function() {
 			this.elements[0].classList.add('is-anim', 'is-loaded');
-			this.elements[0].addEventListener('transitionend', iv.loadedEnd);
+			this.elements[0].addEventListener('transitionend', lz.loadedEnd);
 		},
 		loadedEnd: function() {
 			this.classList.remove('is-anim');
 			this.removeAttribute('data-src');
-			this.removeEventListener('transitionend', iv.loadedEnd);
+			this.removeEventListener('transitionend', lz.loadedEnd);
 		}
 	};
 	
+	/* MEMO: lopan=jp&sample=test みたいなパラメーターをオブジェクトにして返す */
+	function returnObject(param) {
+		const arg = new Object;
+		const arr = param.split('&');
+		for (let i in arr) {
+			var keyval = arr[i].split('=');
+			arg[keyval[0]] = keyval[1];
+		}
+		return arg;
+	}
 	
 	/* MEMO: stroke-dasharray と stroke-dashoffset のスタイルを生成する */
 	function setPathLen() {
